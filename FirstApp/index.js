e43814be1a8016c4f2b3e1af5c293232884f2743
malubsi <mlguimaraess@gmail.com>
2016-10-09 02:30:37 -0300

    Adicionando Plataforma

---
+++ FirstApp/index.js
@@ -0,0 +1,11 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+app.on('ready', function(){
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+})
